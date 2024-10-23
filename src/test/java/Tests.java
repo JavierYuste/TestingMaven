9405804ea3edabc3a404f87f9f5d4d5055afa156
Javier Yuste <javier.yuste@urjc.es>
2024-10-23 18:05:41 +0200

    Update Tests.java

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -7,6 +7,14 @@ public class Tests {
     @Test
     public void testInstance1() {
         double score = Main.experiment("src/main/resources/instances/instance1.txt");
+        System.out.println(score);
+        Assertions.assertTrue(score >= 0.0);
+    }
+
+    @Test
+    public void testInstance2() {
+        double score = Main.experiment("src/main/resources/instances/instance2.txt");
+        System.out.println(score);
         Assertions.assertTrue(score >= 0.0);
     }
 }
